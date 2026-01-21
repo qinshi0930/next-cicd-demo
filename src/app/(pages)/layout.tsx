8a1845cbfb4117153112096488db7a579cd23410
qinshi0930 <music0930@163.com>
2026-01-21 15:30:40 +0800

    Add ICP footer to layout component

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+		<footer className="flex justify-center">
+			<p>ICP备案号：</p>
+			<a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2025078961号</a></footer>
       </body>
     </html>
   );
